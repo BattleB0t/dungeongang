086699ba510ba45c64ea4025d3c441fcb75a33d3
nick <69434290+lelboatz@users.noreply.github.com>
2021-05-20 15:32:05 -0400

    verify command
    finished verify command + fixed verified json to support pushing users

--- commands/bot/verify.js
+++ commands/bot/verify.js
@@ -4,36 +4,79 @@ let prefix = '❮❯'
 
 methods.verify = async function(message, args, config, fs){
     let verified = JSON.parse(fs.readFileSync('./commands/bot/verified.json'))
-    if(!args[1]){
-        return message.channel.send(createErrorEmbed('No username provided.'))
-    }
-    let uuid = await getUUID(args[1])
-    if(uuid == 'invalid player'){
-        return message.channel.send(createErrorEmbed('Invalid username!'))
-    }
+    message.delete()
+    let username
+    let originalMessage = message
     if(message.channel.id != config.discord.verification_channel){
         return message.channel.send(createErrorEmbed('Please use this command in <#'+ config.discord.verification_channel +'>'))
     }
     if(message.member.roles.cache.find(r => r.id === config.discord.member_role) && verified.user_ids.includes(message.author.id)){
         return message.channel.send(createErrorEmbed('You are already verified!'))
     }
-    if(verified.user_ids.includes(message.author.id)){
-        message.channel.send(createErrorEmbed('You are already verified, giving member role...'))
-        return message.member.roles.add(r => r.id == config.discord.member_role)
-    }
-    let verifiedJson = {
-        "[message.author.id]": {
-            "uuid": "uuid",
-            "emotes": {
-                "unlocked_emotes": [],
-                "slots": [
-                    {
-                       "slot_id": 1,
-                       "emote": "none", 
-                       "type": "free"
-                    }
-                ]
-            }
+    if (!args[1]) {
+        try {
+            username = message.member.displayName;
+            username = username.split(" ")[1]
+            username = username.replace(/\W/g, '');
+        } catch (error) {
+            return message.channel.send(createErrorEmbed('An error has occurred while getting this user\'s username'))
         }
+    } else {
+        username = args[1];
     }
+    return message.channel.send(
+        new Discord.MessageEmbed()
+            .setColor('0x00bfff')
+            .setDescription('Grabbing data from API...')
+    )
+        .then(async message => {
+            let uuid = await getUUID(username)
+            if (uuid == 'invalid player') {
+                return message.edit(createErrorEmbed('This player does not exist!'))
+            }
+            let data = await findStats(uuid)
+            if (data[0] == 'error') {
+                data.shift()
+                return message.edit(createErrorEmbed(data.toString()))
+            }
+            let linkedDiscord = await getDiscordFromPlayer(uuid)
+            if (linkedDiscord == "Player does not have a linked discord") {
+                return message.edit(createErrorEmbed('You must link your discord in hypixel!'))
+            } else if (linkedDiscord != tag) {
+                return message.edit(createErrorEmbed('That minecraft account is connected to a different discord!'))
+            }
+            let cataLevel = Math.floor(data.catacombs)
+            originalMessage.member.giveCorrectCataRole(cataLevel)
+            if(verified.user_ids.includes(message.author.id)){
+                message.channel.send(createSuccessEmbed('You are already verified, giving member role...'))
+                try {
+                    await originalMessage.member.setNickname(`❮${cataLevel}❯ ${username}`)
+                } catch (e) { console.log(e) }
+                return message.member.roles.add(r => r.id === config.discord.member_role)
+            }
+            try {
+                await originalMessage.member.setNickname(`❮${cataLevel}❯ ${username}`)
+            } catch (e) { console.log(e) }
+            let verifiedJson = {
+                [message.author.id]: {
+                  "uuid": uuid,
+                  "emotes": {
+                    "unlocked_emotes": [],
+                    "slots": [
+                      {
+                        "slot_id": 1,
+                        "emote": "none",
+                        "type": "default"
+                      }
+                    ]
+                  }
+                }
+              }
+            verified.users.push(verifiedJson)
+            message.edit(createSuccessEmbed(`Verified as **${username}** with catacombs level **${cataLevel}**!`));
+            await sleep(15000)
+            return message.delete()
+        })
 }
+
+exports.data = methods
