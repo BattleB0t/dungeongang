45bc43376d19a4e903dce31af511f7c8efaaba63
alon1396 <75540941+alon1396@users.noreply.github.com>
2021-05-21 16:38:57 +0300

    Update clown.js

--- commands/bot/clown.js
+++ commands/bot/clown.js
@@ -8,28 +8,28 @@ module.exports = {
     description: 'Adds/removes a user from the clown list',
     async execute(message, args, config, fs) {
         if(!message.member.roles.cache.has(config.discord.staff_role)){
-            return message.channel.sendError('You do not have permission to use this command!')
+            return message.channel.send('You do not have permission to use this command!')
         }
         if(args[1] == 'add'){
             let username;
             if(!args[2]){
-                return message.channel.sendError('No username provided.')
+                return message.channel.send('No username provided.')
             }else if(message.mentions.members.first()){
                 try{
                     username = message.mentions.members.first().displayName;
                     username = username.split(" ")[1]
                     username = username.replace(/\W/g, '');
                 }catch(error){
-                    return message.channel.sendError('An error has occurred while getting this user\'s username')
+                    return message.channel.send('An error has occurred while getting this user\'s username')
                 }
             }else{ 
                 username = args[2];
             }
             let uuid = await getUUID(username)
-            if(uuid == 'invalid player') return message.channel.sendError('This player does not exist!')
+            if(uuid == 'invalid player') return message.channel.send('This player does not exist!')
             let jsonString = fs.readFileSync('./data/clowns.json')
             let json = JSON.parse(jsonString)
-            if(json.users.includes(uuid)) return message.channel.sendError('This player is already on the clown list!')
+            if(json.users.includes(uuid)) return message.channel.send('This player is already on the clown list!')
             json.users.push(uuid);
             fs.writeFileSync('./data/clowns.json', JSON.stringify(json, null, 2))
             return message.channel.send(createSuccessEmbed(await getIGN(uuid) +` has been added to the clown list!`))
@@ -37,23 +37,23 @@ module.exports = {
         if(args[1] == 'remove'){
             let username;
             if(!args[2]){
-                return message.channel.sendError('No username provided.')
+                return message.channel.send('No username provided.')
             }else if(message.mentions.members.first()){
                 try{
                     username = message.mentions.members.first().displayName;
                     username = username.split(" ")[1]
                     username = username.replace(/\W/g, '');
                 }catch(error){
-                    return message.channel.sendError('An error has occurred while getting this user\'s username')
+                    return message.channel.send('An error has occurred while getting this user\'s username')
                 }
             }else{ 
                 username = args[2];
             }
             let uuid = await getUUID(username)
-            if(uuid == 'invalid player') return message.channel.sendError('This player does not exist!')
+            if(uuid == 'invalid player') return message.channel.send('This player does not exist!')
             let jsonString = fs.readFileSync('./data/clowns.json')
             let json = JSON.parse(jsonString)
-            if(!json.users.includes(uuid)) return message.channel.sendError('This player is not on the clown list!')
+            if(!json.users.includes(uuid)) return message.channel.send('This player is not on the clown list!')
             let index = json.users.indexOf(uuid)
             if(index > -1){
                 json.users.splice(index, 1)
