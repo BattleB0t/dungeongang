230b4af20fd292846892b061797c027655c22aa7
alon1396 <75540941+alon1396@users.noreply.github.com>
2021-05-22 01:22:59 +0300

    Update apiFunctions.js

--- functions/apiFunctions.js
+++ functions/apiFunctions.js
@@ -46,7 +46,7 @@ global.getIGN = async function getIGN(uuid) {
 }
 global.findStats = async function findStats(uuid) {
     try {
-        function fmtMSS(s) { return (s - (s %= 60)) / 60 + (9 < s ? ':' : ':0') + s }
+
 
         response = await HypixelApiRequest(`https://hypixel-api.senither.com/v1/profiles/${uuid}/weight?key=` + config.minecraft.apiKey, 2)
         if (response === "Api throttle") { return response; }
@@ -156,26 +156,123 @@ global.findStats = async function findStats(uuid) {
     }
 }
 
-global.getMaster = async function getMaster(profileID, uuid) {
-    let mojang = await axios.get('https://sessionserver.mojang.com/session/minecraft/profile/' + uuid)
-    uuid = mojang.data.id
+global.dungeoneering_xp = {
+    0: 0,
+    1: 50,
+    2: 75,
+    3: 110,
+    4: 160,
+    5: 230,
+    6: 330,
+    7: 470,
+    8: 670,
+    9: 950,
+    10: 1340,
+    11: 1890,
+    12: 2665,
+    13: 3760,
+    14: 5260,
+    15: 7380,
+    16: 10300,
+    17: 14400,
+    18: 20000,
+    19: 27600,
+    20: 38000,
+    21: 52500,
+    22: 71500,
+    23: 97000,
+    24: 132000,
+    25: 180000,
+    26: 243000,
+    27: 328000,
+    28: 445000,
+    29: 600000,
+    30: 800000,
+    31: 1065000,
+    32: 1410000,
+    33: 1900000,
+    34: 2500000,
+    35: 3300000,
+    36: 4300000,
+    37: 5600000,
+    38: 7200000,
+    39: 9200000,
+    40: 12000000,
+    41: 15000000,
+    42: 19000000,
+    43: 24000000,
+    44: 30000000,
+    45: 38000000,
+    46: 48000000,
+    47: 60000000,
+    48: 75000000,
+    49: 93000000,
+    50: 116250000
+}
 
-    let response = await HypixelApiRequest(`https://api.hypixel.net/skyblock/profile?key=${config.minecraft.apiKey}&profile=${profileID}`)
+global.getCataAndPb = async function (uuid) {
+    let response = await HypixelApiRequest(`https://api.hypixel.net/skyblock/profiles?key=${config.minecraft.apiKey}&uuid=${uuid}`)
     if (response === "Api throttle") { return response; }
-    masterPB = response?.data?.profile?.members[uuid]?.dungeons?.dungeon_types?.master_catacombs?.fastest_time_s_plus?.[6] || "No PB Found"
-    function fmtMStoMSS(millis) {
-        var minutes = Math.floor(millis / 60000);
-        var seconds = ((millis % 60000) / 1000).toFixed(0);
-        return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
+    response = response.data
+
+    let highestCataXp = -1;
+    let highestProfile = ""
+    for (let i = 0; i < response.profiles.length; i++) {
+        if (response.profiles[i].members[uuid].dungeons !== undefined) {
+            if (response.profiles[i].members[uuid].dungeons.dungeon_types.catacombs.experience > highestCataXp) {
+                highestCataXp = response.profiles[i].members[uuid].dungeons.dungeon_types.catacombs.experience;
+                highestProfile = response.profiles[i];
+            }
+        }
+    };
+    if (highestProfile === "") { return "no dungeon data found" }
+    let cataLevel = 0
+    while (highestCataXp > dungeoneering_xp[cataLevel]) {
+        cataLevel++
+        highestCataXp = highestCataXp - dungeoneering_xp[cataLevel]
     }
-    let json = {}
-    if (masterPB == "No PB Found") {
-        json.masterPB = masterPB
-    } else {
-        json.masterPB = fmtMStoMSS(masterPB)
+    let dataJSON = {
+        cataLevel: cataLevel
     }
-    json.masterPBSeconds = masterPB
-    return json
+
+    for (let i = 0; i <= 7; i++) {
+        dataJSON["F" + i.toString()] = { "s": "No PB Found", "sPlus": "No PB Found" }
+
+        if (highestProfile?.members[uuid]?.dungeons?.dungeon_types?.catacombs?.fastest_time_s[i] || null) {
+            dataJSON["F" + i.toString()]["s"] = fmtMStoMSS(highestProfile.members[uuid].dungeons.dungeon_types.catacombs.fastest_time_s[i])
+        }
+        if (highestProfile?.members[uuid]?.dungeons?.dungeon_types?.catacombs?.fastest_time_s_plus[i] || null) {
+            dataJSON["F" + i.toString()]["sPlus"] = fmtMStoMSS(highestProfile.members[uuid].dungeons.dungeon_types.catacombs.fastest_time_s_plus[i])
+        }
+
+
+        dataJSON["M" + i.toString()] = { "s": "No PB Found", "sPlus": "No PB Found" }
+
+        if (highestProfile?.members[uuid]?.dungeons?.dungeon_types?.master_catacombs?.fastest_time_s[i] || null) {
+            dataJSON["M" + i.toString()]["s"] = fmtMStoMSS(highestProfile.members[uuid].dungeons.dungeon_types.master_catacombs.fastest_time_s[i])
+        }
+        if (highestProfile?.members[uuid]?.dungeons?.dungeon_types?.master_catacombs?.fastest_time_s_plus[i] || null) {
+            dataJSON["M" + i.toString()]["sPlus"] = fmtMStoMSS(highestProfile.members[uuid].dungeons.dungeon_types.master_catacombs.fastest_time_s_plus[i])
+        }
+    }
+    return dataJSON
+}
+
+global.getSecretCountAndCata = async function(uuid) {
+    let response = await HypixelApiRequest(`https://api.hypixel.net/player?key=${config.minecraft.apiKey}&uuid=${uuid}`)
+    if (response === "Api throttle") { return response; }
+    response = response.data
+
+    return {
+        "secretCount": response?.player?.achievements?.skyblock_treasure_hunter || 0,
+        "cata": (response?.player?.achievements?.skyblock_dungeoneer || 0)-1
+    }
+}
+
+global.fmtMStoMSS = function(millis) {
+    var minutes = Math.floor(millis / 60000);
+    var seconds = ((millis % 60000) / 1000).toFixed(0);
+    return minutes + ":" + (seconds < 10 ? '0' : '') + seconds;
 }
 
 global.getDiscordFromPlayer = async function (uuid) {
