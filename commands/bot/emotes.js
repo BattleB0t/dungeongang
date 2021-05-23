ebff006cfb16d809135b677ffc0d3e6892c7f17d
alon1396 <75540941+alon1396@users.noreply.github.com>
2021-05-23 19:25:49 +0300

    Update emotes.js

--- commands/bot/emotes.js
+++ commands/bot/emotes.js
@@ -6,7 +6,7 @@ module.exports = {
     async execute(message, args, config, fs) {
         let verified = JSON.parse(fs.readFileSync('./data/verified.json'))
         if(args.length == 1){
-            if(!verified.user_ids.includes(message.author.id)) return message.channel.send(createErrorEmbed('You have not yet verified with Dungeon Gang v2!'))
+            if(!verified.user_ids.includes(message.author.id)) return message.channel.send(createErrorEmbed(`This user has not yet verified with Dungeon Gang v2! \n Verify by doing -update in <#${config.discord.update_channel}>`))
             await updateAvailableEmotes(message)
             await updateAvailableSlots(message)
             let emotes = JSON.parse(fs.readFileSync('./data/verified.json'))
