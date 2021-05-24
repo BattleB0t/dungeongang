02ca1a0ad496918c66121f1de698c4f120d014d8
alon1396 <75540941+alon1396@users.noreply.github.com>
2021-05-24 22:14:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const commandFolders = fs.readdirSync('./commands');
 const apiFunctions = require('./functions/apiFunctions.js')
 const miscFunctions = require('./functions/miscFunctions.js')
 
+
 for (const folder of commandFolders) {
     const commandFiles = fs
         .readdirSync(`./commands/${folder}`)
@@ -18,21 +19,43 @@ for (const folder of commandFolders) {
         client.commands.set(command.name, command);
     }
 }
-
 client.on('ready', async () => {
     console.log(`Starting Dungeon Gang Bot v1.0.0`);
     client.user.setActivity(`Nice PB Kid`, {
         type: 'PLAYING'
     }).catch(console.error);
-    setInterval(async function(){
+    setInterval(async function () {
         await checkExpiredPolls();
     }, 60 * 1000);
+    setInterval(() => {
+        if (MessagesToBeDeletedVerify.length > 0) {
+            let copyArr = MessagesToBeDeletedVerify
+            MessagesToBeDeletedVerify = []
+            copyArr[0].channel.bulkDelete(copyArr)
+            
+        }
+        if (MessagesToBeDeletedUpdate.length > 0) {
+            let copyArr = MessagesToBeDeletedUpdate
+            MessagesToBeDeletedUpdate = []
+            copyArr[0].channel.bulkDelete(copyArr)
+        }
+    }, 60 * 1000)
 })
+let MessagesToBeDeletedVerify = []
+let MessagesToBeDeletedUpdate = []
 globalThis.messageParam = new Discord.Message();
 globalThis.argsParam = [""]
 globalThis.configParam = config
 globalThis.fsParam = fs
 client.on('message', async (message) => {
+    if (message.channel.id === config.discord.verification_channel || message.channel.id === config.discord.update_channel) {
+        if (!message.member.roles.cache.has(config.discord.staff_role)) {
+            if (message.channel.id === config.discord.verification_channel)
+            MessagesToBeDeletedVerify.push(message)
+            else
+            MessagesToBeDeletedUpdate.push(message)
+        }
+    }
     const cmdargs = message.content.slice(prefix.length).split(/ +/);
     const commandName = cmdargs.shift().toLowerCase();
     const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
@@ -40,11 +63,11 @@ client.on('message', async (message) => {
     if (!message.guild) return;
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
-    if (command == 'help'){
+    if (command == 'help') {
         messageParam = message
         argsParam = cmdargs
         command.execute()
-    }else{
+    } else {
         let args = message.content.substring(prefix.length).split(" ");
         messageParam = message
         argsParam = args
@@ -55,73 +78,73 @@ client.on('message', async (message) => {
 });
 
 client.on('messageReactionAdd', async (reaction, user) => {
-    if(reaction.partial) await reaction.fetch();
-    if(reaction.message.partial) await reaction.message.fetch();
+    if (reaction.partial) await reaction.fetch();
+    if (reaction.message.partial) await reaction.message.fetch();
     let message = reaction.message
-    if(user.bot) return;
-    if(!message.guild) return;
+    if (user.bot) return;
+    if (!message.guild) return;
     let polledUsers = require('./data/polled_users.json')
-    if(!polledUsers.active_polls.includes(message.id)) return;
-    if(message.guild.member(user).roles.cache.find(r => r.id === config.discord.blacklist_role)) return reaction.users.remove(user).catch(console.error)
-    if(reaction.emoji.name === '👍'){
-        if(!getPoll(message.id).votes_positive.includes(user.id) && !getPoll(message.id).votes_neutral.includes(user.id) && !getPoll(message.id).votes_negative.includes(user.id)){
+    if (!polledUsers.active_polls.includes(message.id)) return;
+    if (message.guild.member(user).roles.cache.find(r => r.id === config.discord.blacklist_role)) return reaction.users.remove(user).catch(console.error)
+    if (reaction.emoji.name === '👍') {
+        if (!getPoll(message.id).votes_positive.includes(user.id) && !getPoll(message.id).votes_neutral.includes(user.id) && !getPoll(message.id).votes_negative.includes(user.id)) {
             writePoll(message.id, user.id, "positive")
             return reaction.users.remove(user).catch(console.error)
-        }else if(getPoll(message.id).votes_neutral.includes(user.id)){
+        } else if (getPoll(message.id).votes_neutral.includes(user.id)) {
             unWritePoll(message.id, user.id, "neutral")
             writePoll(message.id, user.id, "positive")
             return reaction.users.remove(user).catch(console.error)
-        }else if(getPoll(message.id).votes_negative.includes(user.id)){
+        } else if (getPoll(message.id).votes_negative.includes(user.id)) {
             unWritePoll(message.id, user.id, "negative")
             writePoll(message.id, user.id, "positive")
             return reaction.users.remove(user).catch(console.error)
-        }else if(getPoll(message.id).votes_positive.includes(user.id)){
+        } else if (getPoll(message.id).votes_positive.includes(user.id)) {
             return reaction.users.remove(user).catch(console.error)
         }
-    }else if(reaction.emoji.name === '🤐'){
-        if(!getPoll(message.id).votes_positive.includes(user.id) && !getPoll(message.id).votes_neutral.includes(user.id) && !getPoll(message.id).votes_negative.includes(user.id)){
+    } else if (reaction.emoji.name === '🤐') {
+        if (!getPoll(message.id).votes_positive.includes(user.id) && !getPoll(message.id).votes_neutral.includes(user.id) && !getPoll(message.id).votes_negative.includes(user.id)) {
             writePoll(message.id, user.id, "neutral")
             return reaction.users.remove(user).catch(console.error)
-        }else if(getPoll(message.id).votes_positive.includes(user.id)){
+        } else if (getPoll(message.id).votes_positive.includes(user.id)) {
             unWritePoll(message.id, user.id, "positive")
             writePoll(message.id, user.id, "neutral")
             return reaction.users.remove(user).catch(console.error)
-        }else if(getPoll(message.id).votes_negative.includes(user.id)){
+        } else if (getPoll(message.id).votes_negative.includes(user.id)) {
             unWritePoll(message.id, user.id, "negative")
             writePoll(message.id, user.id, "neutral")
             return reaction.users.remove(user).catch(console.error)
-        }else if(getPoll(message.id).votes_neutral.includes(user.id)){
+        } else if (getPoll(message.id).votes_neutral.includes(user.id)) {
             return reaction.users.remove(user).catch(console.error)
         }
-    }else if(reaction.emoji.name === '👎'){
-        if(!getPoll(message.id).votes_positive.includes(user.id) && !getPoll(message.id).votes_neutral.includes(user.id) && !getPoll(message.id).votes_negative.includes(user.id)){
+    } else if (reaction.emoji.name === '👎') {
+        if (!getPoll(message.id).votes_positive.includes(user.id) && !getPoll(message.id).votes_neutral.includes(user.id) && !getPoll(message.id).votes_negative.includes(user.id)) {
             writePoll(message.id, user.id, "negative")
             return reaction.users.remove(user).catch(console.error)
-        }else if(getPoll(message.id).votes_positive.includes(user.id)){
+        } else if (getPoll(message.id).votes_positive.includes(user.id)) {
             unWritePoll(message.id, user.id, "positive")
             writePoll(message.id, user.id, "negative")
             return reaction.users.remove(user).catch(console.error)
-        }else if(getPoll(message.id).votes_neutral.includes(user.id)){
+        } else if (getPoll(message.id).votes_neutral.includes(user.id)) {
             unWritePoll(message.id, user.id, "neutral")
             writePoll(message.id, user.id, "negative")
             return reaction.users.remove(user).catch(console.error)
-        }else if(getPoll(message.id).votes_negative.includes(user.id)){
+        } else if (getPoll(message.id).votes_negative.includes(user.id)) {
             return reaction.users.remove(user).catch(console.error)
         }
     }
 })
 
-global.endPollEmbed = async function(message_id, channel_id, EditedPollEmbed){
-    client.channels.cache.get(channel_id).messages.fetch({around: message_id, limit: 1})
-    .then(message => {
-    let fetched_message = message.first();
-    fetched_message.edit({ embed: EditedPollEmbed });
-    fetched_message.reactions.removeAll().catch(error => console.error('Failed to clear reactions for: '+ message.id, error));
-    })
-    .catch(error => {
-        console.log('Unable to edit message: '+ message_id +' in channel: '+ channel_id)
-        console.log(error)
-    })
+global.endPollEmbed = async function (message_id, channel_id, EditedPollEmbed) {
+    client.channels.cache.get(channel_id).messages.fetch({ around: message_id, limit: 1 })
+        .then(message => {
+            let fetched_message = message.first();
+            fetched_message.edit({ embed: EditedPollEmbed });
+            fetched_message.reactions.removeAll().catch(error => console.error('Failed to clear reactions for: ' + message.id, error));
+        })
+        .catch(error => {
+            console.log('Unable to edit message: ' + message_id + ' in channel: ' + channel_id)
+            console.log(error)
+        })
 }
 
 client.login(token)
