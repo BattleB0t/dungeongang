ce92251351b57665f39b67ea61bd0138fbf4378a
alon1396 <75540941+alon1396@users.noreply.github.com>
2021-05-25 16:33:20 +0300

    Update miscFunctions.js

--- functions/miscFunctions.js
+++ functions/miscFunctions.js
@@ -472,4 +472,4 @@ globalThis.createEmoteEmbed2 = async function (user) {
 
 Discord.GuildMember.prototype.isOwner = function () {
   return (this.user.id === "347884694408265729" || this.user.id === "343129897360949248")
-}
+};
