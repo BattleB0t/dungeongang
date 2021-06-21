445730eb2cd17bcf15024b81fbe3950eb3d6906b
nick <69434290+lelboatz@users.noreply.github.com>
2021-06-21 13:21:13 -0400

    Create move.js

---
+++ commands/bot/move.js
@@ -0,0 +1,26 @@
+module.exports = {
+    name: 'move',
+    aliases: ['m'],
+    usage: 'move [user]',
+    description: 'moves a user to your current voice channel',
+    async execute() {
+        let message = messageParam, args = argsParam, config = configParam, fs = fsParam
+        if (!message.member.roles.cache.has('856575336014807041')) {
+            return message.channel.send(createErrorEmbed('You do not have permission to use this command!'))
+        }
+        if(!args[1]){
+            return message.channel.send(createErrorEmbed('Please mention a valid user!'))
+        }
+        if(!message.mentions.members.first()){
+            return message.channel.send(createErrorEmbed('Please mention a valid user!'))
+        }
+        if (!message.member.voice.channel) {
+            return message.channel.send(createErrorEmbed('You must be in a voice channel to use this command!'))
+        }
+        if (!message.mentions.members.first().voice.channel) {
+            return message.channel.send(createErrorEmbed('The user you are moving must be in a voice channel!'))
+        }
+        await message.mentions.members.first().voice.setChannel(message.member.voice.channel, 'Moved into the channel by '+ message.author.tag)
+        return message.channel.send(createSuccessEmbed('Moved '+ args[1] +' into the channel <#'+ message.member.voice.channel.id +'>'))
+    },
+};
