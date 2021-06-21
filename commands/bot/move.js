85c415a00c12bc9af77954291a438a9135e4b9be
nick <69434290+lelboatz@users.noreply.github.com>
2021-06-21 15:47:50 -0400

    Update move.js

--- commands/bot/move.js
+++ commands/bot/move.js
@@ -5,6 +5,16 @@ module.exports = {
     description: 'moves a user to your current voice channel',
     async execute() {
         let message = messageParam, args = argsParam, config = configParam, fs = fsParam
+        if (args[2] === 'to' && message.mentions.channels.first() && message.member.roles.cache.has(config.discord.staff_role)) {
+            if(!message.mentions.members.first()){
+                return message.channel.send(createErrorEmbed('Please mention a valid user!'))
+            }
+            if (!message.mentions.members.first().voice.channel) {
+                return message.channel.send(createErrorEmbed('The user you are moving must be in a voice channel!'))
+            }
+            await message.mentions.members.first().voice.setChannel(message.mentions.channels.first(), 'Moved '+ message.mentions.members.first().user.tag +' into the channel '+ message.mentions.channels.first().name +' by '+ message.author.tag)
+            return message.channel.send(createSuccessEmbed('Moved '+ args[1] +' into the channel <#'+ message.mentions.channels.first().id +'>'))
+        }
         if (!message.member.roles.cache.has('856575336014807041')) {
             return message.channel.send(createErrorEmbed('You do not have permission to use this command!'))
         }
